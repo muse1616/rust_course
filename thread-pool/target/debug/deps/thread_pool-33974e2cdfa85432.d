8d094a603f985575064eeb0d99e7b1db40df819c
jiajun.hu <jiajun.hu@shopee.com>
2023-05-05 11:44:07 +0800

    add simple thread-pool

---
+++ thread-pool/target/debug/deps/thread_pool-33974e2cdfa85432.d
@@ -0,0 +1,5 @@
+/Users/jiajun.hu/github/rust_course/thread-pool/target/debug/deps/thread_pool-33974e2cdfa85432.rmeta: src/lib.rs
+
+/Users/jiajun.hu/github/rust_course/thread-pool/target/debug/deps/thread_pool-33974e2cdfa85432.d: src/lib.rs
+
+src/lib.rs:
---
+++ thread-pool/target/debug/deps/thread_pool-37a388808affbd83.d
@@ -0,0 +1,7 @@
+/Users/jiajun.hu/github/rust_course/thread-pool/target/debug/deps/thread_pool-37a388808affbd83.rmeta: src/lib.rs
+
+/Users/jiajun.hu/github/rust_course/thread-pool/target/debug/deps/libthread_pool-37a388808affbd83.rlib: src/lib.rs
+
+/Users/jiajun.hu/github/rust_course/thread-pool/target/debug/deps/thread_pool-37a388808affbd83.d: src/lib.rs
+
+src/lib.rs:
